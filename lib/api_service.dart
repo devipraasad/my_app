621b2f2a770eac541ad0d4d9588b20ea79bee6b7
scorpio7637 <scorpioo7637@gmail.com>
2026-02-11 19:36:00 +0530

    added aa.dart file

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = "http://192.168.1.19"; // for Android emulator
+  static const String baseUrl = "http://192.168.1.19"; // 
 
   static Future<bool> register(String email, String password) async {
     final response = await http.post(
